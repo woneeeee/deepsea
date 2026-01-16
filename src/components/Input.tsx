69ee205fe5a9ec77f0626a7ed6a4e93404966004
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-01-16 18:56:35 +0900

    feat: input 컴포넌트 구현

---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+// import clsx from 'clsx';
+
+// const Input = () => {
+//   return (
+//     // <div
+//     //   className={clsx(
+//     //     'bg-gray-0 tablet:body-6 body-8 tablet:px-8 tablet:py-5 flex flex-col rounded-md border border-gray-300 px-5 py-4 transition-all duration-200',
+//     //   )}
+//     // >
+//     //   <textarea
+//     //     ref={textareaRef}
+//     //     value={value}
+//     //     onChange={(e) => onChange(e.target.value)}
+//     //     placeholder="질문에 대한 답을 입력해주세요(최대 250자)"
+//     //     className="flex w-full resize-none overflow-hidden border border-none text-gray-700 placeholder-gray-500 focus:outline-none"
+//     //     rows={2}
+//     //   />
+//     //   <span
+//     //     className={clsx(
+//     //       'body-10 tablet:body-8 flex h-6 justify-end text-gray-500 transition-colors duration-200',
+//     //       isOverLimit && 'text-red-100',
+//     //     )}
+//     //   >
+//     //     {charCount}/{maxLength}자
+//     //   </span>
+//     // </div>
+//   );
+// };
+
+// export default Input;
