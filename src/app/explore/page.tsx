7500def06f82c895944842ae8c2accad43ac2ee1
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-01-21 23:05:20 +0900

    feat: end 모달 화면 추가

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -4,6 +4,7 @@ import { useState, useRef, useEffect } from 'react';
 import { useModal } from '@/contexts/ModalContext';
 import ProgressRail from '@/components/ProgressRail';
 import Header from '@/components/Header';
+import EndModal from '@/components/EndModal';
 
 const Page = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -17,6 +18,7 @@ const Page = () => {
   const [selectedIconPosition, setSelectedIconPosition] = useState<{ x: number; y: number } | null>(
     null,
   );
+  const [isEndModalOpen, setIsEndModalOpen] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
   const imageRef = useRef<HTMLImageElement | null>(null);
   const { openModal, isOpen } = useModal();
@@ -140,44 +142,30 @@ const Page = () => {
   }, []);
 
   return (
-    <div
-      ref={containerRef}
-      className="bg-explore relative h-screen w-screen overflow-hidden"
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-      style={{
-        cursor: isDragging ? 'grabbing' : 'grab',
-        backgroundPosition:
-          position.x === 0 ? 'center center' : `calc(50% + ${position.x}px) center`,
-        backgroundSize: 'auto 100%',
-        backgroundRepeat: 'no-repeat',
-        width: '100%',
-        height: '100vh',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-      }}
-    >
-      <Header />
+    <>
       <div
+        ref={containerRef}
+        className="bg-explore relative h-screen w-screen overflow-hidden"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
         style={{
-          position: 'absolute',
+          cursor: isDragging ? 'grabbing' : 'grab',
+          backgroundPosition:
+            position.x === 0 ? 'center center' : `calc(50% + ${position.x}px) center`,
+          backgroundSize: 'auto 100%',
+          backgroundRepeat: 'no-repeat',
+          width: '100%',
+          height: '100vh',
+          position: 'fixed',
           top: 0,
           left: 0,
-          width: '100%',
-          height: '100%',
-          background: `radial-gradient(circle 146px at ${mousePosition.x}px ${mousePosition.y}px, transparent 0%, rgba(0, 0, 0, 0.8) 100%)`,
-          pointerEvents: 'none',
-          zIndex: 1,
-          opacity: isOpen ? 0.8 : 1,
-          transition: 'opacity 0.3s ease',
         }}
-      />
-      {selectedIconPosition && (
+      >
+        <Header />
         <div
           style={{
             position: 'absolute',
@@ -185,75 +173,97 @@ const Page = () => {
             left: 0,
             width: '100%',
             height: '100%',
-            background: `radial-gradient(circle 200px at ${selectedIconPosition.x}px ${selectedIconPosition.y}px, transparent 0%, rgba(0, 0, 0, 0) 100%)`,
+            background: `radial-gradient(circle 146px at ${mousePosition.x}px ${mousePosition.y}px, transparent 0%, rgba(0, 0, 0, 0.8) 100%)`,
             pointerEvents: 'none',
-            zIndex: 2,
+            zIndex: 1,
+            opacity: isOpen ? 0.8 : 1,
+            transition: 'opacity 0.3s ease',
           }}
         />
-      )}
-      {isMounted &&
-        (() => {
-          const imageDisplayWidth = screenSize.height * SVG_ASPECT_RATIO;
-          const imageOffsetX = position.x;
+        {selectedIconPosition && (
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              background: `radial-gradient(circle 200px at ${selectedIconPosition.x}px ${selectedIconPosition.y}px, transparent 0%, rgba(0, 0, 0, 0) 100%)`,
+              pointerEvents: 'none',
+              zIndex: 2,
+            }}
+          />
+        )}
+        {isMounted &&
+          (() => {
+            const imageDisplayWidth = screenSize.height * SVG_ASPECT_RATIO;
+            const imageOffsetX = position.x;
 
-          const iconPositions = [
-            { top: 370, x: SVG_WIDTH - 2170, width: 150, height: 50 }, // emotion1
-            { top: 440, x: SVG_WIDTH - 190, width: 50, height: 148 }, // emotion2
-            { top: 650, x: 122, width: 100, height: 50 }, // emotion3
-            { top: 800, x: 1400, width: 100, height: 50 }, // emotion4
-            { top: 770, x: SVG_WIDTH - 650, width: 130, height: 157 }, // emotion5
-            { top: 1095, x: 585, width: 90, height: 50 }, //emotion6
-            { top: 1000, x: 2038, width: 120, height: 50 }, //emotion7
-            { top: 950, x: 3460, width: 170, height: 150 }, //emotion8
-            { top: 1250, x: 140, width: 100, height: 50 }, //emotion9
-            { top: 1370, x: 4790, width: 90, height: 50 }, //emotion10
-            { top: 1650, x: 670, width: 100, height: 50 }, //emotion11
-            { top: 1800, x: 2231, width: 130, height: 50 }, //emotion12
-            { top: 2050, x: 3280, width: 80, height: 50 }, //emotion13
-            { top: 1830, x: 4360, width: 100, height: 50 }, //emotion14
-            { top: 2080, x: 4900, width: 80, height: 50 }, //emotion15
-          ];
+            const iconPositions = [
+              { top: 370, x: SVG_WIDTH - 2170, width: 150, height: 50 }, // emotion1
+              { top: 440, x: SVG_WIDTH - 190, width: 50, height: 148 }, // emotion2
+              { top: 650, x: 122, width: 100, height: 50 }, // emotion3
+              { top: 800, x: 1400, width: 100, height: 50 }, // emotion4
+              { top: 770, x: SVG_WIDTH - 650, width: 130, height: 157 }, // emotion5
+              { top: 1095, x: 585, width: 90, height: 50 }, //emotion6
+              { top: 1000, x: 2038, width: 120, height: 50 }, //emotion7
+              { top: 950, x: 3460, width: 170, height: 150 }, //emotion8
+              { top: 1250, x: 140, width: 100, height: 50 }, //emotion9
+              { top: 1370, x: 4790, width: 90, height: 50 }, //emotion10
+              { top: 1650, x: 670, width: 100, height: 50 }, //emotion11
+              { top: 1800, x: 2231, width: 130, height: 50 }, //emotion12
+              { top: 2050, x: 3280, width: 80, height: 50 }, //emotion13
+              { top: 1830, x: 4360, width: 100, height: 50 }, //emotion14
+              { top: 2080, x: 4900, width: 80, height: 50 }, //emotion15
+            ];
 
-          return iconPositions.map((iconPos, index) => {
-            const scaledTop = iconPos.top * scaleRatio;
-            const scaledX = iconPos.x * scaleRatio;
+            return iconPositions.map((iconPos, index) => {
+              const scaledTop = iconPos.top * scaleRatio;
+              const scaledX = iconPos.x * scaleRatio;
 
-            const imageCenterX = screenSize.width / 2 + imageOffsetX;
-            const iconScreenX = imageCenterX - imageDisplayWidth / 2 + scaledX;
+              const imageCenterX = screenSize.width / 2 + imageOffsetX;
+              const iconScreenX = imageCenterX - imageDisplayWidth / 2 + scaledX;
 
-            return (
-              <div
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: `${scaledTop}px`,
-                  left: `${iconScreenX}px`,
-                  cursor: 'pointer',
-                }}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  // 클릭된 아이콘의 화면 위치 저장
-                  const rect = e.currentTarget.getBoundingClientRect();
-                  setSelectedIconIndex(index);
-                  setSelectedIconPosition({
-                    x: rect.left + rect.width / 2,
-                    y: rect.top + rect.height / 2,
-                  });
-                  openModal(<div />, index);
-                }}
-              >
-                <img
-                  src={`/icons/emotion${index + 1}.svg`}
-                  alt={`emotion${index + 1}`}
-                  width={iconPos.width}
-                  height={iconPos.height}
-                />
-              </div>
-            );
-          });
-        })()}
-      <ProgressRail />
-    </div>
+              return (
+                <div
+                  key={index}
+                  style={{
+                    position: 'absolute',
+                    top: `${scaledTop}px`,
+                    left: `${iconScreenX}px`,
+                    cursor: 'pointer',
+                  }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    // 클릭된 아이콘의 화면 위치 저장
+                    const rect = e.currentTarget.getBoundingClientRect();
+                    setSelectedIconIndex(index);
+                    setSelectedIconPosition({
+                      x: rect.left + rect.width / 2,
+                      y: rect.top + rect.height / 2,
+                    });
+                    openModal(<div />, index);
+                  }}
+                >
+                  <img
+                    src={`/icons/emotion${index + 1}.svg`}
+                    alt={`emotion${index + 1}`}
+                    width={iconPos.width}
+                    height={iconPos.height}
+                  />
+                </div>
+              );
+            });
+          })()}
+        <ProgressRail onEndClick={() => setIsEndModalOpen(true)} />
+      </div>
+
+      <EndModal
+        isOpen={isEndModalOpen}
+        onContinue={() => setIsEndModalOpen(false)}
+        onEnd={() => setIsEndModalOpen(false)}
+      />
+    </>
   );
 };
 
