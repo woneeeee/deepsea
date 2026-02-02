3135adc95a18ba3ed47e551f950abc8f0afb3885
Kimjiwon <jiwonkim0531@gachon.ac.kr>
2026-02-02 14:31:20 +0900

    버튼 disabled 추가

--- src/app/main/name/page.tsx
+++ src/app/main/name/page.tsx
@@ -39,9 +39,10 @@ const Page = () => {
         placeholder="2자 이상 입력해 주세요."
       />
       <Button
-        className={`pretendard-body-18 mt-[130px] h-[44px] w-[161px] items-center justify-center rounded-[50px] bg-[#E5EDF9]/30 p-[10px] text-center shadow-[0_4px_30px_0_rgba(0,176,192,0.2)] hover:bg-[#BBE7EF99] hover:text-[#00A4C3] active:bg-[#00A4C399] ${
-          inputValue.trim() ? 'text-[#00A4C3]' : 'text-primary-200'
+        className={`pretendard-body-18 disabled:hover:text-primary-200 mt-[130px] h-[44px] w-[161px] items-center justify-center rounded-[50px] bg-[#E5EDF9]/30 p-[10px] text-center shadow-[0_4px_30px_0_rgba(0,176,192,0.2)] hover:bg-[#BBE7EF99] hover:text-[#00A4C3] active:bg-[#00A4C399] disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-[#E5EDF9]/30 disabled:active:bg-[#E5EDF9]/30 ${
+          inputValue.trim().length >= 2 ? 'text-[#00A4C3]' : 'text-primary-200'
         }`}
+        disabled={inputValue.trim().length < 2}
         onClick={saveUsername}
       >
         탐험 하러 가기
